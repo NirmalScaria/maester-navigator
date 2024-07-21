9b23fc14279cd32cb1939874996ebf930925ca00
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-21 20:45:47 +0530

    [FEATURE] Continents and bounds

--- src/pages/Popup/geoJsonMap.tsx
+++ src/pages/Popup/geoJsonMap.tsx
@@ -1,9 +1,10 @@
-import { LatLngExpression } from 'leaflet';
+import L, { LatLngExpression, LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import { GeoJSON, MapContainer, Marker } from 'react-leaflet';
-import { currentLocationMarker } from './elements';
+import React, { useEffect } from 'react';
+import { GeoJSON, MapContainer, Marker, useMap, useMapEvents } from 'react-leaflet';
+import { ContinentLabels, CoordinatesCopier, currentLocationMarker, TextLabel } from './elements';
 import geojsonData from './map.json';
+import places from './data/places.json'
 
 
 const currentLocation: LatLngExpression = [52.107017102899, -32.4634878401302];
@@ -18,12 +19,14 @@ const style = (feature: any) => {
   };
 };
 
-
 const GeoJsonMap: React.FC = () => {
+
   return (
-    <MapContainer attributionControl={false} center={currentLocation} zoom={5} style={{ height: '100%', width: '100%', margin: 0, padding: 0, backgroundColor: '#66e1e3' }}>
+    <MapContainer minZoom={2.32} maxBoundsViscosity={10000} maxBounds={new L.LatLngBounds([-3.8369702166630044, -55.84149843858185], [71.61030647250153, 125.86686650752802])} zoomSnap={0.7} attributionControl={false} center={currentLocation} zoom={5} style={{ height: '100%', width: '100%', margin: 0, padding: 0, backgroundColor: '#66e1e3' }}>
       <GeoJSON data={geojsonData as any} style={style} />
       <Marker position={currentLocation} icon={currentLocationMarker} />
+      <ContinentLabels />
+      <CoordinatesCopier />
     </MapContainer>
   );
 };
