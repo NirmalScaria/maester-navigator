4d7bbd1abf15cbe4d3901c0e2c920a3c6dded3f0
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-21 21:19:00 +0530

    [FEATURE] Cities and Castles

--- src/pages/Popup/geoJsonMap.tsx
+++ src/pages/Popup/geoJsonMap.tsx
@@ -2,12 +2,15 @@ import L, { LatLngExpression, LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import React, { useEffect } from 'react';
 import { GeoJSON, MapContainer, Marker, useMap, useMapEvents } from 'react-leaflet';
-import { ContinentLabels, CoordinatesCopier, currentLocationMarker, TextLabel } from './elements';
+import { CastleLabels, CityLabels, ContinentLabels, CoordinatesCopier, currentLocationMarker, TextLabel } from './elements';
 import geojsonData from './map.json';
 import places from './data/places.json'
 
 
-const currentLocation: LatLngExpression = [52.107017102899, -32.4634878401302];
+const currentLocation: LatLngExpression = [
+  52.107017102899,
+  -32.4634878401302
+]
 
 const style = (feature: any) => {
   return {
@@ -20,13 +23,34 @@ const style = (feature: any) => {
 };
 
 const GeoJsonMap: React.FC = () => {
+  const [levelsToShow, setLevelsToShow] = React.useState<string[]>(["continent"]);
 
+  function ZoomSetter() {
+    useMapEvents({
+      zoom: (e) => {
+        if (e.target.getZoom() < 3) {
+          setLevelsToShow(["continent"]);
+        } else {
+          if(e.target.getZoom() >= 4) {
+            setLevelsToShow(["city", "castle"])
+          }
+          else {
+            setLevelsToShow(["city"])
+          }
+        }
+      }
+    })
+    return null
+  }
   return (
     <MapContainer minZoom={2.32} maxBoundsViscosity={10000} maxBounds={new L.LatLngBounds([-3.8369702166630044, -55.84149843858185], [71.61030647250153, 125.86686650752802])} zoomSnap={0.7} attributionControl={false} center={currentLocation} zoom={5} style={{ height: '100%', width: '100%', margin: 0, padding: 0, backgroundColor: '#66e1e3' }}>
       <GeoJSON data={geojsonData as any} style={style} />
       <Marker position={currentLocation} icon={currentLocationMarker} />
-      <ContinentLabels />
+      {levelsToShow.includes('continent') && <ContinentLabels />}
+      {levelsToShow.includes('city') && <CityLabels />}
+      {levelsToShow.includes('castle') && <CastleLabels />}
       <CoordinatesCopier />
+      <ZoomSetter />
     </MapContainer>
   );
 };
