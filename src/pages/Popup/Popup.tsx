be428961f74a24a2ec8eaf57f6fbfea5076cfd45
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-22 12:25:39 +0530

    [FEATURE] Show live location

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
 import './Popup.css';
 import GeoJsonMap from './map';
+import episodes from './data/knownEpisodes.json'
+import locations from './data/knownLocations.json' assert { type: "json" };
+import { LatLngExpression } from 'leaflet';
 
 const Popup = () => {
   const [videoDetails, setVideoDetails] = React.useState<any>(null);
+  const [content, setContent] = React.useState<any>(null);
+  const [currentLocation, setCurrentLocation] = React.useState<LatLngExpression | null>(null);
   function getDetails() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.scripting.executeScript(
@@ -22,13 +27,25 @@ const Popup = () => {
           }
         },
         (results) => {
-          console.log(results)
           if (results && results[0] && results[0].result) {
             setVideoDetails(results[0].result);
+            getLocation(results[0].result);
           }
         }
       );
     });
+  }
+
+  function getLocation({ currentTime }: { currentTime: number }) {
+    const episode = episodes[1][6]
+    const scenes = episode.scenes
+    const currentScene = scenes!.find((scene: any) => stringToNum(scene.start) <= currentTime && stringToNum(scene.end) >= currentTime)
+    if (currentScene && currentScene.location) {
+      const locationName: string = currentScene.location
+      // @ts-ignore
+      const location: any = locations[locationName]
+      setCurrentLocation(location)
+    }
   }
 
   useEffect(() => {
@@ -39,9 +56,19 @@ const Popup = () => {
 
   return (
     <div className="App">
-      <GeoJsonMap />
+      <GeoJsonMap currentLocation={currentLocation} />
     </div>
   );
 };
+
+function stringToNum(time: string) {
+  // Takes time like '1:23:45' and returns number of seconds
+  var res = 0;
+  const parts = time.split(':');
+  for (let i = 0; i < parts.length; i++) {
+    res += parseInt(parts[i], 10) * Math.pow(60, parts.length - 1 - i);
+  }
+  return res;
+}
 
 export default Popup;
