a9badbf14b3a576a85b37fc9ed2884d9d6b97cd9
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-07-22 12:25:39 +0530

    [FEATURE] Show live location

--- src/pages/Popup/map.tsx
+++ src/pages/Popup/map.tsx
@@ -1,34 +1,30 @@
 import L, { LatLngExpression, Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ImageOverlay, MapContainer, Marker, useMapEvents } from 'react-leaflet';
 import { CoordinatesCopier, currentLocationMarker } from './elements';
 // @ts-ignore
 import mapImage from "../../assets/img/mapImage.jpg";
 
 
-const currentLocation: LatLngExpression = [33.38395654179022, -27.678533609484532]
+const defaultLocation: LatLngExpression = [33.38395654179022, -27.678533609484532]
 
-const style = (feature: any) => {
-  return {
-    color: '#fafaf588', // Line color
-    weight: 2, // Line thickness
-    fillColor: '#6bcf94', // Polygon fill color
-    fillOpacity: 0.5, // Polygon fill opacity
-    stroke: true
-  };
-};
-
-const GeoJsonMap: React.FC = () => {
+function GeoJsonMap({ currentLocation }: { currentLocation: LatLngExpression | null }) {
   const [levelsToShow, setLevelsToShow] = React.useState<string[]>(["city", "castle", "town", "ruin"]);
+  const [center, setCenter] = React.useState<LatLngExpression>(currentLocation ?? defaultLocation);
   const mapRef = useRef<Map>(null);
 
-
+  useEffect(() => {
+    if (currentLocation && center !== currentLocation) {
+      setCenter(currentLocation);
+      mapRef.current?.setView(currentLocation, 5);
+    }
+  })
 
   return (
-    <MapContainer minZoom={2.32} maxZoom={7} maxBoundsViscosity={10000} maxBounds={new L.LatLngBounds([-4.369702166630044, -60.84149843858185], [61.29030647250153, 125.86686650752802])} ref={mapRef} zoomSnap={0.7} attributionControl={false} center={currentLocation} zoom={5} style={{ height: '100%', width: '100%', margin: 0, padding: 0, backgroundColor: '#66e1e3' }}>
+    <MapContainer minZoom={2.32} maxZoom={7} maxBoundsViscosity={10000} maxBounds={new L.LatLngBounds([-4.369702166630044, -60.84149843858185], [61.29030647250153, 125.86686650752802])} ref={mapRef} zoomSnap={0.7} attributionControl={false} center={currentLocation ?? defaultLocation} zoom={5} style={{ height: '100%', width: '100%', margin: 0, padding: 0, backgroundColor: '#66e1e3' }}>
       {/* <GeoJSON data={geojsonData as any} style={style} /> */}
-      <Marker position={currentLocation} icon={currentLocationMarker} />
+      {currentLocation && <Marker position={currentLocation} icon={currentLocationMarker} />}
       <ImageOverlay url={mapImage} bounds={[[-20, -66], [72, 130]]} />
       <CoordinatesCopier />
       <ZoomSetter />
